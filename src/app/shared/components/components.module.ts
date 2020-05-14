23821b49d01bef72bfa6d98e7fd8441796934751
Rachid Guemili <rachidwilder33@gmail.com>
2020-05-14 10:25:00 +0200

    feat/Sidebar

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -5,6 +5,7 @@ import {RouterModule} from "@angular/router";
 import {FooterComponent} from "./footer/footer.component";
 import {SidebarComponent} from "./sidebar/sidebar.component";
 import { HeaderComponent } from './header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
     exports: [
         FooterComponent,
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee, faAddressCard, faHamburger, faUser, faBars, faTimes, faDiceD20, faClinicMedical, faPeopleCarry, faHospitalSymbol } from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
 
-  constructor() { }
+
+export class SidebarComponent implements OnInit {
+  constructor(library: FaIconLibrary) {
+    
+    library.addIcons(faCoffee);
+    library.addIcons(faPeopleCarry);
+    library.addIcons(faUser);
+    library.addIcons(faAddressCard);
+    library.addIcons(faDiceD20);
+    library.addIcons(faBars);
+    library.addIcons(faTimes);
+    library.addIcons(faHospitalSymbol);
+    
+  }
 
   ngOnInit(): void {
   }
 
 }
+
